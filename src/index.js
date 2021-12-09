04346caa46f3478bba98249f7ca4f3bd4285bb1d
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-12-09 11:44:59 -0600

    commit tree 373f489e269e2d773a33ed91ec81ea75c9f4c142

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
-  // write your createStore code here
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState,
+  }
 }
 
-export function candyReducer(state = [], action) {
+function candyReducer(state = [], action) {
   switch (action.type) {
     case "candies/add":
       return [...state, action.candy];
@@ -22,5 +36,8 @@ function render() {
   }
 }
 
+
+let store = createStore(candyReducer)
+store.dispatch({ type: "@@INIT" })
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
